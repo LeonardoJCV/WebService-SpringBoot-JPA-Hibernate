7229724ea2a54f4dc9ad4e840f3d644b9dd85b85
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-01 17:07:14 -0300

    Order, Instant and ISO 8601

--- src/main/java/com/leonardojcv/springboot/entities/User.java
+++ src/main/java/com/leonardojcv/springboot/entities/User.java
@@ -1,12 +1,15 @@
 package com.leonardojcv.springboot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 //Classe de usúario
@@ -24,6 +27,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	//Anotação @OneToMany informando ao Spring a relação de um para muitos mapeado pelo "client"
+	@OneToMany(mappedBy = "client")
+	private List<Order>orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -75,6 +82,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
