1976112ad95acea5ef827ea45603260f004241c1
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-01 18:38:43 -0300

    OrderStatus enum

--- src/main/java/com/leonardojcv/springboot/config/TestConfig.java
+++ src/main/java/com/leonardojcv/springboot/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.leonardojcv.springboot.entities.Order;
 import com.leonardojcv.springboot.entities.User;
+import com.leonardojcv.springboot.entities.enums.OrderStatus;
 import com.leonardojcv.springboot.repositories.OrderRepository;
 import com.leonardojcv.springboot.repositories.UserRepository;
 
@@ -35,9 +36,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		//no Terceiro parametro, o usuario entra no construtor e faz a associação entre objetos
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1); 
 		
 		//Método para salvar as instancias em forma de lista no banco de dados
 		userRepository.saveAll(Arrays.asList(u1, u2));
--- src/main/java/com/leonardojcv/springboot/entities/Order.java
+++ src/main/java/com/leonardojcv/springboot/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.leonardojcv.springboot.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,6 +31,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	//Anotações @ManyToOne e @JoinColumn para indicar ao Spring que trata-se de uma relação muitos para um e FK
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -39,9 +42,10 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -67,6 +71,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
