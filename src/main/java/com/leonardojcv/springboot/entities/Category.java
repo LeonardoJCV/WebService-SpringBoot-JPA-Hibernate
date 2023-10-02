bdc8164bbbee4a18f4a5e9a3f0c98c264d96ae13
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-02 19:07:49 -0300

    ManyToMany association with joinTable

--- src/main/java/com/leonardojcv/springboot/entities/Category.java
+++ src/main/java/com/leonardojcv/springboot/entities/Category.java
@@ -5,12 +5,14 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 //Anotação @Entity indicando que esta class é uma entidade
 @Entity
@@ -25,7 +27,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
+	// Anotação para associação de mapeamento, referente a classe Product
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product>products = new HashSet<>();
 	
 	public Category() {
