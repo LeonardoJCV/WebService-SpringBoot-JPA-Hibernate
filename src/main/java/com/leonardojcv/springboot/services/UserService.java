63657b84de329a31a72afb1afec3a53d10737598
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-05 18:23:23 -0300

    Exception handling - delete

--- src/main/java/com/leonardojcv/springboot/services/UserService.java
+++ src/main/java/com/leonardojcv/springboot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.leonardojcv.springboot.entities.User;
 import com.leonardojcv.springboot.repositories.UserRepository;
+import com.leonardojcv.springboot.services.exceptions.DatabaseException;
 import com.leonardojcv.springboot.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +37,13 @@ public class UserService {
 	
 	//Operação básica para deleção do objeto "user"
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	//Operação básica para atualização do objeto "user"
