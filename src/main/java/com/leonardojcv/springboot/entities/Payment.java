68eac791672a2edf59d8536c7e0dcdef714058d8
Leonardo Vieira <leonardojcv@Outlook.com>
2023-10-05 16:48:04 -0300

    Sobtotal e Total methods

--- src/main/java/com/leonardojcv/springboot/entities/Payment.java
+++ src/main/java/com/leonardojcv/springboot/entities/Payment.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +28,7 @@ public class Payment implements Serializable {
 	private Instant moment;
 
 	//Associação @OneToOne 1 para 1 na classe dependente
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
